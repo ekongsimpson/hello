ba03dc3c6cfe1ef57c57a6b3e290faf546cfd04b
ekongsimpson <ekong.simpson@gmail.com>
2025-06-03 16:10:27 +0200

    hello - just azure

--- k8s-multicloud/main.tf
+++ k8s-multicloud/main.tf
@@ -4,14 +4,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 5.0"
+#    }
+#    google = {
+#      source  = "hashicorp/google"
+#      version = "~> 4.0"
+#    }
   }
 
   required_version = ">= 1.3"
@@ -23,29 +23,29 @@ provider "azurerm" {
 
 }
 
-provider "aws" {
-  region = var.aws_region
-}
-
-provider "google" {
-  credentials = file(var.gcp_sa_key_file)
-  project     = var.gcp_project
-  region      = var.gcp_region
-}
+#provider "aws" {
+#  region = var.aws_region
+#}
+#
+#provider "google" {
+#  credentials = file(var.gcp_sa_key_file)
+#  project     = var.gcp_project
+#  region      = var.gcp_region
+#}
 
 module "azure" {
   source = "./azure"
   location = var.azure_location
 }
 
-module "aws" {
-  source     = "./aws"
-  region     = var.aws_region
-  subnet_ids = var.aws_subnet_ids
-}
-
-module "gcp" {
-  source         = "./gcp"
-  region         = var.gcp_region
-  gke_node_count = 2
-}
+#module "aws" {
+#  source     = "./aws"
+#  region     = var.aws_region
+#  subnet_ids = var.aws_subnet_ids
+#}
+#
+#module "gcp" {
+#  source         = "./gcp"
+#  region         = var.gcp_region
+#  gke_node_count = 2
+#}
--- k8s-multicloud/variables.tf
+++ k8s-multicloud/variables.tf
@@ -1,28 +1,28 @@
-variable "aws_region" {
-  default = "eastus-1"
-}
-
-
-
-variable "aws_subnet_ids" {
-  type = list(string)
-  default = ["subnet-12345678", "subnet-87654321"]
-}
-
-variable "gcp_region" {
-  default = "us-east1"
-}
-
-variable "gcp_project" {
-  default = "gcp-project-id"
-  
-}
-
-variable "gcp_sa_key_file" {
-  description = "Path to GCP service account key file"
-  type        = string
-  #default     = "terraform-sa-key.json"
-}
+#variable "aws_region" {
+#  default = "eastus-1"
+#}
+#
+#
+#
+#variable "aws_subnet_ids" {
+#  type = list(string)
+#  default = ["subnet-12345678", "subnet-87654321"]
+#}
+#
+#variable "gcp_region" {
+#  default = "us-east1"
+#}
+#
+#variable "gcp_project" {
+#  default = "gcp-project-id"
+#  
+#}
+#
+#variable "gcp_sa_key_file" {
+#  description = "Path to GCP service account key file"
+#  type        = string
+#  #default     = "terraform-sa-key.json"
+#}
 
 variable "azure_location" {
   description = "Azure region"
