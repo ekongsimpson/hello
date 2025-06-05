d36b5b6f2847250ad5edb8090c5055b58f1c6963
ekongsimpson <ekong.simpson@gmail.com>
2025-06-05 14:58:24 +0200

    hello - loadbalancer

--- k8s-multicloud/gcp/variables.tf
+++ k8s-multicloud/gcp/variables.tf
@@ -2,7 +2,6 @@ variable "region" {
   description = "GCP region"
   type        = string
 }
-
 variable "gke_node_count" {
   description = "Initial number of nodes in GKE cluster"
   type        = number
--- k8s-multicloud/main.tf
+++ k8s-multicloud/main.tf
@@ -4,14 +4,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "~> 5.0"
-#    }
-#    google = {
-#      source  = "hashicorp/google"
-#      version = "~> 4.0"
-#    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
   }
 
   required_version = ">= 1.3"
@@ -20,32 +20,48 @@ terraform {
 provider "azurerm" {
   features {}
 
-
 }
 
-#provider "aws" {
-#  region = var.aws_region
-#}
-#
+provider "aws" {
+  region = var.aws_region
+}
+
 #provider "google" {
 #  credentials = file(var.gcp_sa_key_file)
 #  project     = var.gcp_project
 #  region      = var.gcp_region
 #}
 
+module "gcp" {
+  source         = "./gcp"
+  region         = var.gcp_region
+  gke_node_count = 2
+}
+
 module "azure" {
   source = "./azure"
   location = var.azure_location
 }
 
-#module "aws" {
-#  source     = "./aws"
-#  region     = var.aws_region
-#  subnet_ids = var.aws_subnet_ids
-#}
-#
-#module "gcp" {
-#  source         = "./gcp"
-#  region         = var.gcp_region
-#  gke_node_count = 2
-#}
+module "aws" {
+  source     = "./aws"
+  region     = var.aws_region
+  subnet_ids = var.aws_subnet_ids
+}
+
+resource "aws_iam_role" "eks-role" {
+  name = "eks-role"  # Is this a hardcoded string or interpolated?
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
